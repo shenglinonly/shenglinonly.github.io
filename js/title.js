630286487ba4c1f4440ed94a84cd466bfd6bfe67
shenglinonly <99405721+shenglinonly@users.noreply.github.com>
2022-09-26 05:07:47 +0000

    add configuration Updated By Github Actions

--- js/title.js
+++ js/title.js
@@ -1,14 +1,14 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>//动态标题
+//动态标题
 var OriginTitile = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
+        document.title = '又岂在朝朝暮暮';
         clearTimeout(titleTime);
     } else {
         //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)欢迎肥来！' + OriginTitile;
+        document.title = '还晓得回来' + OriginTitile;
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
