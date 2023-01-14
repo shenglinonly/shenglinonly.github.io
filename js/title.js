219a9f801e0851eccde91b5bdcc0694820d0f661
shiningonly <shenglinonly@gmail.com>
2023-01-14 16:30:21 +0800

    Site updated: 2023-01-14 16:30:21

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
+        document.title = '又岂在朝朝暮暮';
         clearTimeout(titleTime);
     } else {
         //返回当前页面时标签显示内容
-        document.title = '♪(^∇^*)欢迎肥来！' + OriginTitile;
+        document.title = '还晓得回来';
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
